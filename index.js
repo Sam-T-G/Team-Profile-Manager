d133ed597306d2c512baf312d45b1e5259a29761
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-09-14 20:19:02 -0700

    added base values for question arrays

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ const Engineer = require("./lib/Engineer");
 const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 
+const questions = {
+  Manager: [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the manager's name?",
+    },
+  ],
+
+  Engineer: [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?",
+    },
+  ],
+
+  Employee: [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the employee's name?",
+    },
+  ],
+
+  Intern: [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?",
+    },
+  ],
+};
